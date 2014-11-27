36d46cdaa7bc72421173d550bb20ca658232888e
Josh Salverda <josh.salverda@gmail.com>
2014-11-26 23:21:45 -0500

    Add no warranty clause

--- src/datepickr.js
+++ src/datepickr.js
@@ -4,9 +4,11 @@
     https://github.com/joshsalverda/datepickr
 
     Copyright © 2014 Josh Salverda <josh.salverda@gmail.com>
-    This work is free. You can redistribute it and/or modify it under the
-    terms of the Do What The Fuck You Want To Public License, Version 2,
-    as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+    This program is free software. It comes without any warranty, to
+    the extent permitted by applicable law. You can redistribute it
+    and/or modify it under the terms of the Do What The Fuck You Want
+    To Public License, Version 2, as published by Sam Hocevar. See
+    http://www.wtfpl.net/ for more details.
 */
 
 var datepickr = function (selector, config) {
