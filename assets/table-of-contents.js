1fa64cb10dc93f282c0db0731db59974e187f333
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-04-06 23:30:35 -0400

    Table of contents, star/follow/download, style updates

---
+++ assets/table-of-contents.js
@@ -0,0 +1,28 @@
+var table = document.querySelector(".c-r div"),
+    list = table.querySelector(".c-r ol"),
+    links = document.querySelectorAll("[data-desc]"),
+    header_height = document.querySelector(".hero").offsetHeight,
+    onScroll = function(){
+
+        if (document.body.scrollTop > header_height)
+            table.classList.add("fixed");
+        else
+            table.classList.remove("fixed");
+
+
+    };
+
+for (i = 0; i < links.length; ++i) {
+
+    var item = document.createElement("li"),
+        link = document.createElement("a");
+
+    item.innerHTML = "<span>"+ (i+1 + ". ") + "</span>";
+    link.href="#"+links[i].id;
+    link.innerText=links[i].dataset.desc;
+    item.appendChild(link);
+    list.appendChild(item);
+
+}
+onScroll();
+window.addEventListener("scroll", onScroll);
