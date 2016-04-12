b00f7bac26b2badfd236f6f258ca2e1590346ae0
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-04-12 14:09:55 -0400

    gulp4 + babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,52 @@
+import gulp from "gulp"
+import stylus from "gulp-stylus"
+import babel from "gulp-babel"
+import cssmin from "gulp-cssmin"
+import uglify from "gulp-uglify"
+import rename from "gulp-rename"
+
+const paths = {
+    style: "src/style/flatpickr.styl",
+    script: "src/flatpickr.js",
+    themes: "./src/style/themes/*.styl"
+};
+
+export function script() {
+    return gulp.src(paths.script)
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(uglify()).on('error', errorHandler)
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+};
+
+export function style() {
+    return gulp.src(paths.style)
+    .pipe(stylus())
+    .pipe(cssmin()).on('error', errorHandler)
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+};
+
+export function themes() {
+    return gulp.src(paths.themes)
+    .pipe(stylus())
+    .pipe(cssmin()).on('error', errorHandler)
+    .pipe(rename({ prefix: 'flatpickr.',suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+};
+
+export function watch() {
+    gulp.watch('./src/style/*.styl', style);
+    gulp.watch('./src/style/themes/*.styl', themes);
+    gulp.watch(paths.script, script);
+};
+
+
+// Handle the error
+function errorHandler (error) {
+    console.log(error.toString());
+}
+
+const build = gulp.parallel(script, style, themes,watch);
+export {build}
+export default build;
