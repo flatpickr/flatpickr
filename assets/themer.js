2ce6778aadb3b8ae8897f25aadce7189d92e2d3b
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-04-14 18:08:57 -0400

    Nicer theme changer

--- assets/themer.js
+++ assets/themer.js
@@ -5,10 +5,11 @@ var theme_sel = document.getElementById("themes"),
 for(var i = 0; i < themes.length; i++){
     var opt = document.createElement("option");
     opt.value = themes[i];
-    opt.innerText = themes[i].slice(1);
+    opt.innerText = themes[i].slice(1).replace(/_/g," ");
     theme_sel.appendChild(opt);
 }
 
 theme_sel.addEventListener("change", function(e){
     stylesheet.href="dist/flatpickr"+e.target.value+".min.css";
+    theme_sel.blur();
 });
