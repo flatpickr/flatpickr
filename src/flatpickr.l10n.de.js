938d9516b14b8611366b939f39dd4600a733b191
Marian Arlt <MarianArlt@users.noreply.github.com>
2016-06-20 14:49:16 -0500

    Update flatpickr.l10n.de.js

--- src/flatpickr.l10n.de.js
+++ src/flatpickr.l10n.de.js
@@ -1,3 +1,5 @@
+/* German locals for flatpickr */
+
 flatpickr.init.prototype.l10n.weekdays = {
 	shorthand: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
 	longhand: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
