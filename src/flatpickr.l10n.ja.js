3005cf18ae4195ef2bccf69c4f48e9174963e104
Marian Arlt <MarianArlt@users.noreply.github.com>
2016-06-20 14:53:11 -0500

    Added localizations
    For the top ten most spoken languages by native speakers from 2010

---
+++ src/flatpickr.l10n.ja.js
@@ -0,0 +1,11 @@
+/* Japanese locals for flatpickr */
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['日', '月', '火', '水', '木', '金', '土'],
+	longhand: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+	longhand: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+};
---
+++ src/flatpickr.l10n.zh.js
@@ -0,0 +1,11 @@
+/* Mandarin locals for flatpickr */
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+	longhand: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+	longhand: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+};
