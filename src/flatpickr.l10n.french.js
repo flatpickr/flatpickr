c65a7c7f3fea3fa41a51c6d82e307f4f19e8391d
Baptiste Donaux <baptiste.donaux@gmail.com>
2016-06-23 17:41:50 +0200

    [TRANSLATION] add french translation

---
+++ src/flatpickr.l10n.french.js
@@ -0,0 +1,9 @@
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['D', 'L', 'Ma', 'Me', 'J', 'V', 'S'],
+	longhand: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aoû', 'Sept', 'Oct', 'Nov', 'Déc'],
+	longhand: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+};
