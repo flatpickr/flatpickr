9ca15d54afb085f5ba144e08966b8e11ccde697b
Viljami Anttonen <viljami.anttonen@gmail.com>
2016-06-25 12:08:58 +0300

    Added finnish translation

---
+++ src/flatpickr.l10n.fi.js
@@ -0,0 +1,17 @@
+/* Finnish locals for flatpickr */
+
+flatpickr.init.prototype.l10n.firstDayOfWeek = 1;
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
+	longhand: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['Tammi', 'Helmi', 'Maalis', 'Huhti', 'Touko', 'Kesä', 'Heinä', 'Elo', 'Syys', 'Loka', 'Marras', 'Joulu'],
+	longhand: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu']
+};
+
+flatpickr.init.prototype.l10n.ordinal = () => {
+	return ".";
+};
