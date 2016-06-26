e6df05dc05721a6b7c2dc2a055cf95afd31e9888
Sajjad Hashemian <wolaws@gmail.com>
2016-06-26 23:42:29 +0430

    Add Farsi (Persian) locals

---
+++ src/flatpickr.l10n.fa.js
@@ -0,0 +1,15 @@
+/* Farsi (Persian) locals for flatpickr */
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['یک', 'دو', 'سه', 'چهار', 'پنج', 'آدینه', 'شنبه'],
+	longhand: ['یک‌شنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنچ‌شنبه', 'آدینه', 'شنبه']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['ژانویه', 'فوریه', 'مارس', 'آوریل', 'مه', 'ژوئن', 'ژوئیه', 'اوت', 'سپتامبر', 'اکتبر', 'نوامبر', 'دسامبر'],
+	longhand: ['ژانویه', 'فوریه', 'مارس', 'آوریل', 'مه', 'ژوئن', 'ژوئیه', 'اوت', 'سپتامبر', 'اکتبر', 'نوامبر', 'دسامبر']
+};
+
+flatpickr.init.prototype.l10n.ordinal = () => {
+	return "";
+};
