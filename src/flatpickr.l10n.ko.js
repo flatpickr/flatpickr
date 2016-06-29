45a5a4dd056fb3956bc19398fb41625ced5e0ffc
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-29 23:35:29 +0900

    Add korea ordinal function

--- src/flatpickr.l10n.ko.js
+++ src/flatpickr.l10n.ko.js
@@ -9,3 +9,7 @@ flatpickr.init.prototype.l10n.months = {
 	shorthand: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
 	longhand: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월']
 };
+
+flatpickr.init.prototype.l10n.ordinal = () => {
+    return "일";
+};
