1cd7a26c9907e84b193007241421d5a403247a89
Matěj Polák <matejpolak42@gmail.com>
2016-07-08 16:33:48 +0200

    Added czech translation

---
+++ src/flatpickr.i10n.cs.js
@@ -0,0 +1,17 @@
+/* Czech locals for flatpickr */
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
+	longhand: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['Led', 'Ún', 'Bře', 'Dub', 'Kvě', 'Čer', 'Čvc', 'Srp', 'Zář', 'Říj', 'Lis', 'Pro'],
+	longhand: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec']
+};
+
+flatpickr.init.prototype.l10n.firstDayOfWeek = 1;
+
+flatpickr.init.prototype.l10n.ordinal = function() {
+	return ".";
+};
