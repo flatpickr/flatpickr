dc1db2792f3e1f39213f6fe38a9b10fb5bcb2dfa
Martin Haubek <haubek@gmail.com>
2016-07-19 02:03:10 +1000

    Create flatpickr.l10n.da.js (#182)
    Support for Danish locals.

---
+++ src/flatpickr.l10n.da.js
@@ -0,0 +1,15 @@
+/* Danish locals for flatpickr */
+
+flatpickr.init.prototype.l10n.weekdays = {
+	shorthand: ['Søn', 'Man', 'Tir', 'Ons', 'Tors', 'Fre', 'Lør'],
+	longhand: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag']
+};
+
+flatpickr.init.prototype.l10n.months = {
+	shorthand: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+	longhand: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December']
+};
+
+flatpickr.init.prototype.l10n.ordinal = () => {
+	return ".";
+};
