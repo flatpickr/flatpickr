524e00fff2a1ab0cfc5a8aa8e96190fc0459b22b
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-07-24 12:01:58 -0400

    Fix localizations | #195

--- src/flatpickr.i10n.cs.js
+++ src/flatpickr.i10n.cs.js
@@ -1,17 +1,17 @@
 /* Czech locals for flatpickr */
 
-flatpickr.init.prototype.l10n.weekdays = {
+Flatpickr.l10n.weekdays = {
 	shorthand: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
 	longhand: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota']
 };
 
-flatpickr.init.prototype.l10n.months = {
+Flatpickr.l10n.months = {
 	shorthand: ['Led', 'Ún', 'Bře', 'Dub', 'Kvě', 'Čer', 'Čvc', 'Srp', 'Zář', 'Říj', 'Lis', 'Pro'],
 	longhand: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec']
 };
 
-flatpickr.init.prototype.l10n.firstDayOfWeek = 1;
+Flatpickr.l10n.firstDayOfWeek = 1;
 
-flatpickr.init.prototype.l10n.ordinal = function() {
+Flatpickr.l10n.ordinal = function() {
 	return ".";
 };
--- src/flatpickr.l10n.de.js
+++ src/flatpickr.l10n.de.js
@@ -1,11 +1,11 @@
 /* German locals for flatpickr */
 
-flatpickr.init.prototype.l10n.weekdays = {
+Flatpickr.l10n.weekdays = {
 	shorthand: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
 	longhand: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
 };
 
-flatpickr.init.prototype.l10n.months = {
+Flatpickr.l10n.months = {
 	shorthand: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
 	longhand: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
 };
