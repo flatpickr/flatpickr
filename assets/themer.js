f37e5770051c4f8de7b2d25240b2850e4537ba84
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-07-24 15:14:51 -0400

    Two new themes: grapefruit and base16 flat

--- assets/themer.js
+++ assets/themer.js
@@ -1,15 +1,21 @@
 var theme_sel = document.getElementById("themes"),
     stylesheet = document.getElementById("cal_style"),
-    themes = [".material_blue", ".material_red", ".material_orange", ".material_green", ".dark", ".confetti"];
+    themes = [
+    	"material_blue", "material_red", "material_orange", "material_green",
+    	"dark",
+    	"confetti",
+    	"grapefruit",
+    	"base16_flat"
+    ];
 
 for(var i = 0; i < themes.length; i++){
     var opt = document.createElement("option");
     opt.value = themes[i];
-    opt.innerText = themes[i].slice(1).replace(/_/g," ");
+    opt.innerText = themes[i].replace(/_/g," ");
     theme_sel.appendChild(opt);
 }
 
 theme_sel.addEventListener("change", function(e){
-    stylesheet.href="dist/flatpickr"+e.target.value+".min.css";
+    stylesheet.href="dist/flatpickr."+(e.target.value ? (e.target.value+".") : "")+"min.css";
     theme_sel.blur();
 });
