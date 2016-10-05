f71ef258decc28ca0dd409eeb181c33de2605fc0
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-10-05 18:46:37 -0400

    reintroduce themer

--- assets/js/themer.js
+++ assets/js/themer.js
@@ -1,22 +1,22 @@
 
-	var theme_sel = document.getElementById("themes"),
-		stylesheet = document.getElementById("cal_style"),
-		themes = [
-			"material_blue", "material_red", "material_orange", "material_green",
-			"dark",
-			"confetti",
-			"base16_flat"
-		];
+var theme_sel = document.getElementById("themes"),
+	stylesheet = document.getElementById("cal_style"),
+	themes = [
+		"material_blue", "material_red", "material_orange", "material_green",
+		"dark",
+		"confetti",
+		"base16_flat"
+	];
 
-	for(var i = 0; i < themes.length; i++){
-		var opt = document.createElement("option");
-		opt.value = "." + themes[i];
-		opt.innerText = themes[i].replace(/_/g," ");
-		theme_sel.appendChild(opt);
-	}
+for(var i = 0; i < themes.length; i++){
+	var opt = document.createElement("option");
+	opt.value = "." + themes[i];
+	opt.innerText = themes[i].replace(/_/g," ");
+	theme_sel.appendChild(opt);
+}
 
-	theme_sel.addEventListener("change", function(e){
-		stylesheet.href="dist/flatpickr" + e.target.value +".min.css";
-		theme_sel.blur();
-	});
+theme_sel.addEventListener("change", function(e){
+	stylesheet.href="dist/flatpickr" + e.target.value +".min.css";
+	theme_sel.blur();
+});
 
