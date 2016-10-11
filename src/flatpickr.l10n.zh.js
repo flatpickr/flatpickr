495457a79723a9649e93ef883d318a2c518fbdd2
ilvsx <ilvsxk@gmail.com>
2016-10-11 10:48:43 +0800

    Modularize localization for flatpickr.l10n.zh.js

--- src/flatpickr.l10n.zh.js
+++ src/flatpickr.l10n.zh.js
@@ -10,3 +10,7 @@ Flatpickr.l10n.months = {
 	shorthand: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
 	longhand: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
 };
+
+if (typeof module !== "undefined") {
+	module.exports = Flatpickr.l10n;
+}
