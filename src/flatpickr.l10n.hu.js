31390922641bc3aa0435dc0d0225ff8b7f9485d9
Peter Kiss <kicsy87@gmail.com>
2016-10-14 15:47:29 +0200

    Hungarian locale

---
+++ src/flatpickr.l10n.hu.js
@@ -0,0 +1,22 @@
+/* Hungarian locals for flatpickr */
+var Flatpickr = Flatpickr||{l10n: {}};
+
+Flatpickr.l10n.firstDayOfWeek = 1;
+
+Flatpickr.l10n.weekdays = {
+	shorthand: ['V', 'H', 'K', 'Sz', 'Cs', 'P', 'Szo'],
+	longhand: ['Vasárnap', 'Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat']
+};
+
+Flatpickr.l10n.months = {
+	shorthand: ['Jan', 'Feb', 'Már', 'Ápr', 'Máj', 'Jún', 'Júl', 'Aug', 'Szep', 'Okt', 'Nov', 'Dec'],
+	longhand: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+};
+
+Flatpickr.l10n.ordinal = function() {
+	return ".";
+};
+
+if (typeof module !== "undefined") {
+	module.exports = Flatpickr.l10n;
+}
