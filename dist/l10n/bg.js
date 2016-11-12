2ec4bd3638b19799a9bb8c825f17f148c2c9c9da
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-11-12 15:58:27 -0500

    reorganize & transpile localizations

---
+++ dist/l10n/bg.js
@@ -0,0 +1,16 @@
+/* Bulgarian locals for flatpickr */
+var Flatpickr = Flatpickr || { l10ns: {} };
+Flatpickr.l10ns.bg = {};
+
+Flatpickr.l10ns.bg.weekdays = {
+	shorthand: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+	longhand: ['Неделя', 'Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота']
+};
+
+Flatpickr.l10ns.bg.months = {
+	shorthand: ['Яну', 'Фев', 'Март', 'Апр', 'Май', 'Юни', 'Юли', 'Авг', 'Сеп', 'Окт', 'Ное', 'Дек'],
+	longhand: ['Януари', 'Февруари', 'Март', 'Април', 'Май', 'Юни', 'Юли', 'Август', 'Септември', 'Октомври', 'Ноември', 'Декември']
+};
+if (typeof module !== "undefined") {
+	module.exports = Flatpickr.l10ns;
+}
---
+++ dist/l10n/da.js
@@ -0,0 +1,20 @@
+/* Danish locals for flatpickr */
+var Flatpickr = Flatpickr || { l10ns: {} };
+Flatpickr.l10ns.da = {};
+
+Flatpickr.l10ns.da.weekdays = {
+	shorthand: ['Søn', 'Man', 'Tir', 'Ons', 'Tors', 'Fre', 'Lør'],
+	longhand: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag']
+};
+
+Flatpickr.l10ns.da.months = {
+	shorthand: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+	longhand: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December']
+};
+
+Flatpickr.l10ns.da.ordinal = function () {
+	return ".";
+};
+if (typeof module !== "undefined") {
+	module.exports = Flatpickr.l10ns;
+}
