3dcf6fbac1544f88ebccc0f852cb7d0ef7908fd4
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-11-25 00:50:11 -0500

    Hide time input initially until date selection. animate calendar + time

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -653,6 +653,7 @@ function Flatpickr(element, config) {
 			self.mobileInput.value = "";
 
 		self.selectedDates = [];
+		self.dateIsPicked = false;
 
 		triggerEvent("Change");
 	}
@@ -1177,6 +1178,8 @@ function Flatpickr(element, config) {
 		buildDays();
 		triggerEvent("Change");
 
+		setTimeout(() => self.dateIsPicked = true, 50);
+
 		if (self.config.mode === "range" && self.selectedDates.length === 1)
 			onMouseOver(e);
 	}
@@ -1199,6 +1202,8 @@ function Flatpickr(element, config) {
 
 		setHoursFromDate();
 		updateValue();
+
+		self.dateIsPicked = self.selectedDates.length > 0;
 
 		if (triggerChange)
 			triggerEvent("Change");
@@ -1520,7 +1525,7 @@ function Flatpickr(element, config) {
 		e.className = className;
 
 		if (content)
-			e.textContent = content;
+			e.innerHTML = content;
 
 		return e;
 	}
@@ -1684,7 +1689,6 @@ Flatpickr.defaultConfig = {
 	// code for previous/next icons. this is where you put your custom icon code e.g. fontawesome
 	prevArrow: "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
 	nextArrow: "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
-
 
 	// enables seconds in the time picker
 	enableSeconds: false,
