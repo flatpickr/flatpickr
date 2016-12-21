9d7a1083d7a0f043570ac004b2a44500df559e51
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-12-20 23:49:13 -0500

    reorganize repo

--- assets/js/themer.js
+++ assets/js/themer.js
@@ -1,28 +1,30 @@
-var theme_sel = document.getElementById("themes"),
+const theme_sel = document.getElementById("themes"),
 	stylesheet = document.getElementById("cal_style"),
 	themes = [
-		"material_blue", "material_red", "material_orange", "material_green",
 		"dark",
+		"material_blue", "material_red", "material_orange", "material_green",
 		"airbnb",
-		"confetti",
-		"base16_flat"
+		"confetti"
 	];
 
-theme_sel.href = "dist/flatpickr" + themes[Math.floor(Math.random()*themes.length)] + ".min.css";
+//theme_sel.href = "dist/flatpickr" + themes[Math.floor(Math.random()*themes.length)] + ".min.css";
 
-for(var i = 0; i < themes.length; i++){
-	var opt = document.createElement("option");
-	opt.value = "." + themes[i];
+for(let i = 0; i < themes.length; i++){
+	const opt = document.createElement("option");
+	opt.className=themes[i];
+	opt.value = themes[i];
 	opt.innerText = themes[i].replace(/_/g," ");
-
-	if (stylesheet.href.indexOf(opt.value +".min.css") > -1)
-		opt.setAttribute("selected", "");
 
 	theme_sel.appendChild(opt);
 }
 
 theme_sel.addEventListener("change", function(e){
-	stylesheet.href="dist/flatpickr" + e.target.value +".min.css";
 	theme_sel.blur();
+
+	if (!e.target.value || e.target.value === "default")
+		return (stylesheet.href="bower_components/flatpickr/dist/flatpickr.css");
+
+	stylesheet.href="bower_components/flatpickr/dist/themes/" + e.target.value +".css";
+
 });
 
