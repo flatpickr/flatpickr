e908671ee38ab20b6fed3e8f49b288b851946015
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-12-21 22:34:19 -0500

    Account for padding when determining size

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -78,8 +78,8 @@ function Flatpickr(element, config) {
 		}
 
 		if (self.config.weekNumbers) {
-			self.calendarContainer.style.width = self.days.offsetWidth
-				+ self.weekWrapper.offsetWidth + "px";
+			self.calendarContainer.style.width = self.days.clientWidth
+				+ self.weekWrapper.clientWidth + "px";
 		}
 
 
