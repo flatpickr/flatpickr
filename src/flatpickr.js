fdf77ce23cd5269de8ea956f0571a3b099b6eaa9
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-01-15 00:14:20 -0500

    check for e.path.indexOf | resolves #548, closes #549

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -750,7 +750,11 @@ function Flatpickr(element, config) {
 		const isInput = self.element.contains(e.target)
 			|| e.target === self.input
 			|| e.target === self.altInput
-			|| (e.path && (~e.path.indexOf(self.input) || ~e.path.indexOf(self.altInput)));
+			|| (
+				// web components
+				e.path && e.path.indexOf &&
+				(~e.path.indexOf(self.input) || ~e.path.indexOf(self.altInput))
+			);
 
 		if (self.isOpen && !self.config.inline && !isCalendarElem(e.target) && !isInput) {
 			e.preventDefault();
