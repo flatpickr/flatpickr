349785363a3ec2039c295985efc2885cc5e1c926
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-03-21 13:57:02 -0400

    confirmIcon => confirmDate

--- src/plugins/confirmDate/confirmDate.js
+++ src/plugins/confirmDate/confirmDate.js
@@ -1,4 +1,4 @@
-function confirmIconPlugin(pluginConfig) {
+function confirmDatePlugin(pluginConfig) {
 	const defaultConfig = {
 		confirmIcon: "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='17' height='17' viewBox='0 0 17 17'> <g> </g> <path d='M15.418 1.774l-8.833 13.485-4.918-4.386 0.666-0.746 4.051 3.614 8.198-12.515 0.836 0.548z' fill='#000000' /> </svg>",
 		confirmText: "OK ",
@@ -45,4 +45,4 @@ function confirmIconPlugin(pluginConfig) {
 }
 
 if (typeof module !== "undefined")
-	module.exports = confirmIconPlugin;
+	module.exports = confirmDatePlugin;
