d1e544f15c65bb4ea45db351028bee7d76a5dabb
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-03-21 14:05:53 -0400

    confirmDate: add theme class

--- src/plugins/confirmDate/confirmDate.js
+++ src/plugins/confirmDate/confirmDate.js
@@ -2,7 +2,8 @@ function confirmDatePlugin(pluginConfig) {
 	const defaultConfig = {
 		confirmIcon: "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='17' height='17' viewBox='0 0 17 17'> <g> </g> <path d='M15.418 1.774l-8.833 13.485-4.918-4.386 0.666-0.746 4.051 3.614 8.198-12.515 0.836 0.548z' fill='#000000' /> </svg>",
 		confirmText: "OK ",
-		showAlways: false
+		showAlways: false,
+		theme: "light"
 	};
 
 	const config = {};
@@ -10,9 +11,15 @@ function confirmDatePlugin(pluginConfig) {
 		config[key] = pluginConfig[key] !== undefined 
 			? pluginConfig[key] 
 			: defaultConfig[key];
+	}
 
 	return function(fp) {
-		fp.confirmContainer = fp._createElement("div", "flatpickr-confirm", config.confirmText);
+		fp.confirmContainer = fp._createElement(
+			"div", 
+			"flatpickr-confirm " + config.theme + "Theme", 
+			config.confirmText
+		);
+
 		fp.confirmContainer.tabIndex = -1;
 		fp.confirmContainer.innerHTML += config.confirmIcon;
 
