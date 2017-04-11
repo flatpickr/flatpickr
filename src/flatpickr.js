b2d1788aada40002fb65b7a26ee3673a33a6498e
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-04-11 19:09:59 -0400

    remove classList polyfill

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -867,7 +867,7 @@ function Flatpickr(element, config) {
 		triggerEvent("MonthChange");
 
 		if (skipAnimations)
-			return updateNavigationCurrentMonth();;
+			return updateNavigationCurrentMonth();
 		
 		self.oldCurMonth = self.navigationCurrentMonth;
 		self.navigationCurrentMonth = self.monthNav.insertBefore(
@@ -2589,61 +2589,6 @@ Date.prototype.fp_toUTC = function () {
 	newDate.fp_isUTC = true;
 	return newDate;
 };
-
-// IE9 classList polyfill
-/* istanbul ignore next */
-if (
-	!(window.document.documentElement.classList) &&
-	Object.defineProperty && typeof HTMLElement !== "undefined"
-) {
-	Object.defineProperty(HTMLElement.prototype, "classList", {
-		get: function () {
-			let self = this;
-			function update(fn) {
-				return function (value) {
-					let classes = self.className.split(/\s+/),
-						index = classes.indexOf(value);
-
-					fn(classes, index, value);
-					self.className = classes.join(" ");
-				};
-			}
-
-			let ret = {
-				add: update((classes, index, value) => {
-					if (!(~index))
-						classes.push(value);
-				}),
-
-				remove: update((classes, index) => {
-					if (~index)
-						classes.splice(index, 1);
-				}),
-
-				toggle: update((classes, index, value) => {
-					if (~index)
-						classes.splice(index, 1);
-					else
-						classes.push(value);
-				}),
-
-				contains: value => !!~self.className.split(/\s+/).indexOf(value),
-
-				item: function (i) {
-					return self.className.split(/\s+/)[i] || null;
-				}
-			};
-
-			Object.defineProperty(ret, "length", {
-				get: function () {
-					return self.className.split(/\s+/).length;
-				}
-			});
-
-			return ret;
-		}
-	});
-}
 
 if (typeof module !== "undefined")
 	module.exports = Flatpickr;
