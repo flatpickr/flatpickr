781cb813249eba4e5dda17cdc65fdb3caf34a78c
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-04-18 15:07:59 -0400

    build

--- dist/flatpickr.js
+++ dist/flatpickr.js
@@ -894,6 +894,7 @@ function Flatpickr(element, config) {
 			if (lostFocus) {
 				e.preventDefault();
 				self.close();
+				self._input.blur();
 
 				if (self.config.mode === "range" && self.selectedDates.length === 1) {
 					self.clear();
