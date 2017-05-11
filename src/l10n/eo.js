75550e1eb1b34413cc6a4dc9466e0b02b4d50113
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2017-05-11 12:16:10 +0200

    Added l10n/eo.js

---
+++ src/l10n/eo.js
@@ -0,0 +1,27 @@
+/* Esperanto locals for flatpickr */
+var Flatpickr = Flatpickr || {l10ns: {}};
+Flatpickr.l10ns.eo = {};
+
+Flatpickr.l10ns.eo.firstDayOfWeek = 1;
+
+Flatpickr.l10ns.eo.rangeSeparator = " ĝis ";
+Flatpickr.l10ns.eo.weekAbbreviation = "Sem";
+Flatpickr.l10ns.eo.scrollTitle = "Rulumu por pligrandigi la valoron";
+Flatpickr.l10ns.eo.toggleTitle = "Klaku por ŝalti";
+
+Flatpickr.l10ns.eo.weekdays = {
+	shorthand: ["Dim", "Lun", "Mar", "Mer", "Ĵaŭ", "Ven", "Sab"],
+	longhand: ["dimanĉo", "lundo", "mardo", "merkredo", "ĵaŭdo", "vendredo", "sabato"]
+};
+
+Flatpickr.l10ns.eo.months = {
+	shorthand: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aŭg", "Sep", "Okt", "Nov", "Dec"],
+	longhand: ["januaro", "februaro", "marto", "aprilo", "majo", "junio", "julio", "aŭgusto", "septembro", "oktobro", "novembro", "decembro"]
+};
+
+Flatpickr.l10ns.eo.ordinal = () => {
+	return "-a";
+};
+
+if (typeof module !== "undefined") 
+	module.exports = Flatpickr.l10ns;
