90a33804bac4eb6bbcaf2bb809cf40d7baa9d30a
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-05-31 22:12:07 -0400

    build

--- dist/flatpickr.js
+++ dist/flatpickr.js
@@ -3,11 +3,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 /*! flatpickr v2.6.3, @license MIT */
-
-// retain backwards compatibility with v2
-var Flatpickr = flatpickr;
-
-function FlatpickrClass(element, config) {
+function Flatpickr(element, config) {
 	var self = this;
 
 	self._ = {};
@@ -29,8 +25,8 @@ function FlatpickrClass(element, config) {
 	function init() {
 		self.element = self.input = element;
 		self.instanceConfig = config || {};
-		self.parseDate = FlatpickrClass.prototype.parseDate.bind(self);
-		self.formatDate = FlatpickrClass.prototype.formatDate.bind(self);
+		self.parseDate = Flatpickr.prototype.parseDate.bind(self);
+		self.formatDate = Flatpickr.prototype.formatDate.bind(self);
 
 		setupFormats();
 		parseConfig();
@@ -1478,13 +1474,13 @@ function FlatpickrClass(element, config) {
 
 	/* istanbul ignore next */
 	function setupFormats() {
-		self.formats = Object.create(FlatpickrClass.prototype.formats);
+		self.formats = Object.create(Flatpickr.prototype.formats);
 		["D", "F", "J", "M", "W", "l"].forEach(function (f) {
-			self.formats[f] = FlatpickrClass.prototype.formats[f].bind(self);
+			self.formats[f] = Flatpickr.prototype.formats[f].bind(self);
 		});
 
-		self.revFormat.F = FlatpickrClass.prototype.revFormat.F.bind(self);
-		self.revFormat.M = FlatpickrClass.prototype.revFormat.M.bind(self);
+		self.revFormat.F = Flatpickr.prototype.revFormat.F.bind(self);
+		self.revFormat.M = Flatpickr.prototype.revFormat.M.bind(self);
 	}
 
 	function setupInputs() {
@@ -1769,7 +1765,7 @@ function FlatpickrClass(element, config) {
 	return self;
 }
 
-FlatpickrClass.prototype = {
+Flatpickr.prototype = {
 	formats: {
 		// get the date in UTC
 		Z: function Z(date) {
@@ -1788,12 +1784,12 @@ FlatpickrClass.prototype = {
 
 		// padded hour 1-12
 		G: function G(date) {
-			return FlatpickrClass.prototype.pad(FlatpickrClass.prototype.formats.h(date));
+			return Flatpickr.prototype.pad(Flatpickr.prototype.formats.h(date));
 		},
 
 		// hours with leading zero e.g. 03
 		H: function H(date) {
-			return FlatpickrClass.prototype.pad(date.getHours());
+			return Flatpickr.prototype.pad(date.getHours());
 		},
 
 		// day (1-30) with ordinal suffix e.g. 1st, 2nd
@@ -1813,7 +1809,7 @@ FlatpickrClass.prototype = {
 
 		// seconds 00-59
 		S: function S(date) {
-			return FlatpickrClass.prototype.pad(date.getSeconds());
+			return Flatpickr.prototype.pad(date.getSeconds());
 		},
 
 		// unix timestamp
@@ -1832,7 +1828,7 @@ FlatpickrClass.prototype = {
 
 		// day in month, padded (01-30)
 		d: function d(date) {
-			return FlatpickrClass.prototype.pad(date.getDate());
+			return Flatpickr.prototype.pad(date.getDate());
 		},
 
 		// hour from 1-12 (am/pm)
@@ -1842,7 +1838,7 @@ FlatpickrClass.prototype = {
 
 		// minutes, padded with leading zero e.g. 09
 		i: function i(date) {
-			return FlatpickrClass.prototype.pad(date.getMinutes());
+			return Flatpickr.prototype.pad(date.getMinutes());
 		},
 
 		// day in month (1-30)
@@ -1857,7 +1853,7 @@ FlatpickrClass.prototype = {
 
 		// padded month number (01-12)
 		m: function m(date) {
-			return FlatpickrClass.prototype.pad(date.getMonth() + 1);
+			return Flatpickr.prototype.pad(date.getMonth() + 1);
 		},
 
 		// the month number (1-12)
@@ -2066,7 +2062,7 @@ function _flatpickr(nodeList, config) {
 				nodes[i]._flatpickr = null;
 			}
 
-			nodes[i]._flatpickr = new FlatpickrClass(nodes[i], config || {});
+			nodes[i]._flatpickr = new Flatpickr(nodes[i], config || {});
 			instances.push(nodes[i]._flatpickr);
 		} catch (e) {
 			console.warn(e, e.stack);
@@ -2090,7 +2086,6 @@ if (typeof HTMLElement !== "undefined") {
 
 /* istanbul ignore next */
 function flatpickr(selector, config) {
-	console.log(selector);
 	if (selector instanceof NodeList) return _flatpickr(selector, config);else if (!(selector instanceof HTMLElement)) return _flatpickr(window.document.querySelectorAll(selector), config);
 
 	return _flatpickr([selector], config);
