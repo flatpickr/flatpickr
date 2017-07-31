acb0de21dcfd97a2f771829839c8e14f9f623592
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-07-31 10:18:14 -0400

    fix spacing

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -2489,7 +2489,7 @@ FlatpickrInstance.prototype = {
 								val: match[++matchIndex]
 							});
 						}
-						}
+					}
 
 					else if (!isBackSlash)
 						regexStr += "."; // don't really care
