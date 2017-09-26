d4cceb69c11998ff4dc5d0311c92e18f170d4bee
Gregory <gregory.mkv@gmail.com>
2017-09-26 16:01:25 -0400

    Create index declaration

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "flatpickr" {
+  import { FlatpickrFn } from "types/instance";
+  export { Instance } from "types/instance";
+  export { Options, Plugin } from "types/options";
+
+  export default FlatpickrFn;
+}
