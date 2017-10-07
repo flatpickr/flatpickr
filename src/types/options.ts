bbc32f829675ec83890f0038ea355482c4a14931
Gregory <gregory.mkv@gmail.com>
2017-10-06 22:43:10 -0400

    Add comments to some of the options

--- src/types/options.ts
+++ src/types/options.ts
@@ -17,167 +17,197 @@ type Hook = (
 export type Plugin = (fp: Instance) => Options;
 
 export interface Options {
-  mode?: "single" | "multiple" | "range";
-  position?: "auto" | "above" | "below";
-  animate?: boolean;
-  wrap?: boolean;
-  weekNumbers?: boolean;
+  /*
+  Allows the user to enter a date directly input the input field. By default, direct entry is disabled.
+  */
   allowInput?: boolean;
-  clickOpens?: boolean;
-  closeOnSelect?: boolean;
-  time_numberhr?: boolean;
-  enableTime?: boolean;
-  noCalendar?: boolean;
-  dateFormat?: string;
-  ariaDateFormat?: string;
-  altInput?: boolean;
-  altInputClass?: string;
-  altFormat?: string;
-  defaultDate?: DateOption;
-  minDate?: DateOption;
-  maxDate?: DateOption;
-  parseDate?: (date: string, format: string) => Date;
-  formatDate?: (date: Date, format: string) => string;
-  getWeek?: (date: Date) => string | number;
-  enable?: DateLimit[];
-  disable?: DateLimit[];
-  shorthandCurrentMonth?: boolean;
-  inline?: boolean;
 
-  static?: boolean;
+  /* Exactly the same as date format, but for the altInput field */
+  altFormat?: string;
+
+  /* Show the user a readable date (as per altFormat), but return something totally different to the server.*/
+  altInput?: boolean;
+
+  /* This class will be added to the input element created by the altInput option.  Note that altInput already inherits classes from the original input. */
+  altInputClass?: string;
+
+  /* Whether to enable animations, such as month transitions */
+  animate?: boolean;
+
+  /* Instead of body, appends the calendar to the specified node instead. */
   appendTo?: HTMLElement;
-  prevArrow?: string;
-  nextArrow?: string;
-  enableSeconds?: boolean;
-  hourIncrement?: number;
-  minuteIncrement?: number;
+
+  /* Defines how the date will be formatted in the aria-label for calendar days, using the same tokens as dateFormat. If you change this, you should choose a value that will make sense if a screen reader reads it out loud. */
+  /* Defaults to "F j, Y" */
+  ariaDateFormat?: string;
+
+  /*
+    Whether clicking on the input should open the picker.
+    Set it to false if you only want to open the calendar programmatically
+  */
+  clickOpens?: boolean;
+
+  /* Whether calendar should close after date selection */
+  closeOnSelect?: boolean;
+
+  /*
+    A string of characters which are used to define how the date will be displayed in the input box.
+    See https://chmln.github.io/flatpickr/formatting
+  */
+  dateFormat?: string;
+
+  /* The initial selected date(s). */
+  defaultDate?: DateOption | DateOption[];
+
+  /* Initial value of the hour element. */
   defaultHour?: number;
   defaultMinute?: number;
   defaultSeconds?: number;
+  disable?: DateLimit[];
   disableMobile?: boolean;
-  locale?: string | CustomLocale;
-  plugins?: Plugin[];
+  enable?: DateLimit[];
+  enableSeconds?: boolean;
+  enableTime?: boolean;
+  formatDate?: (date: Date, format: string) => string;
+  getWeek?: (date: Date) => string | number;
+  hourIncrement?: number;
   ignoredFocusElements?: HTMLElement[];
-  onClose?: Hook | Hook[];
+  inline?: boolean;
+  locale?: string | CustomLocale;
+  maxDate?: DateOption;
+  minDate?: DateOption;
+  minuteIncrement?: number;
+  mode?: "single" | "multiple" | "range";
+  nextArrow?: string;
+  noCalendar?: boolean;
   onChange?: Hook | Hook[];
+  onClose?: Hook | Hook[];
   onDayCreate?: Hook | Hook[];
+  onDestroy?: Hook | Hook[];
+  onKeyDown?: Hook | Hook[];
   onMonthChange?: Hook | Hook[];
   onOpen?: Hook | Hook[];
   onParseConfig?: Hook | Hook[];
-  onReady?: Hook | Hook[]; // function (dateObj dateStr) {}
+  onReady?: Hook | Hook[];
   onValueUpdate?: Hook | Hook[];
   onYearChange?: Hook | Hook[];
-  onKeyDown?: Hook | Hook[];
-  onDestroy?: Hook | Hook[];
+  parseDate?: (date: string, format: string) => Date;
+  plugins?: Plugin[];
+  position?: "auto" | "above" | "below";
+  prevArrow?: string;
+  shorthandCurrentMonth?: boolean;
+  static?: boolean;
   time_24hr?: boolean;
+  time_numberhr?: boolean;
+  weekNumbers?: boolean;
+  wrap?: boolean;
 }
 
 export interface ParsedOptions {
   [k: string]: any;
-  mode: "single" | "multiple" | "range";
-  position: "auto" | "below" | "above";
-  animate: boolean;
-  wrap: boolean;
-  weekNumbers: boolean;
+  _disable: DateLimit<Date>[];
+  _enable: DateLimit<Date>[];
   allowInput: boolean;
-  clickOpens: boolean;
-  closeOnSelect: boolean;
-  time_24hr: boolean;
-  enableTime: boolean;
-  noCalendar: boolean;
-  dateFormat: string;
-  ariaDateFormat: string;
+  altFormat: string;
   altInput: boolean;
   altInputClass: string;
-  altFormat: string;
-  defaultDate?: Date;
-  minDate?: Date;
-  maxDate?: Date;
-  parseDate?: Options["parseDate"];
-  formatDate?: Options["formatDate"];
-  getWeek: (date: Date) => string | number;
-  _enable: DateLimit<Date>[];
-  enable: DateLimit<Date>[];
-  _disable: DateLimit<Date>[];
-  disable: DateLimit<Date>[];
-  shorthandCurrentMonth: boolean;
-  inline: boolean;
-  static: boolean;
+  animate: boolean;
   appendTo?: HTMLElement;
-  prevArrow: string;
-  nextArrow: string;
-  enableSeconds: boolean;
-  hourIncrement: number;
-  minuteIncrement: number;
+  ariaDateFormat: string;
+  clickOpens: boolean;
+  closeOnSelect: boolean;
+  dateFormat: string;
+  defaultDate?: Date;
   defaultHour: number;
   defaultMinute: number;
   defaultSeconds: number;
+  disable: DateLimit<Date>[];
   disableMobile: boolean;
-  locale: string | CustomLocale;
-  plugins: Plugin[];
+  enable: DateLimit<Date>[];
+  enableSeconds: boolean;
+  enableTime: boolean;
+  formatDate?: Options["formatDate"];
+  getWeek: (date: Date) => string | number;
+  hourIncrement: number;
   ignoredFocusElements: HTMLElement[];
-  onClose: Hook[];
+  inline: boolean;
+  locale: string | CustomLocale;
+  maxDate?: Date;
+  minDate?: Date;
+  minuteIncrement: number;
+  mode: "single" | "multiple" | "range";
+  nextArrow: string;
+  noCalendar: boolean;
   onChange: Hook[];
+  onClose: Hook[];
   onDayCreate: Hook[];
+  onDestroy: Hook[];
+  onKeyDown: Hook[];
   onMonthChange: Hook[];
   onOpen: Hook[];
   onParseConfig: Hook[];
   onReady: Hook[];
   onValueUpdate: Hook[];
   onYearChange: Hook[];
-  onKeyDown: Hook[];
-  onDestroy: Hook[];
+  parseDate?: Options["parseDate"];
+  plugins: Plugin[];
+  position: "auto" | "below" | "above";
+  prevArrow: string;
+  shorthandCurrentMonth: boolean;
+  static: boolean;
+  time_24hr: boolean;
+  weekNumbers: boolean;
+  wrap: boolean;
 }
 
 export const defaults: ParsedOptions = {
-  mode: "single",
-  position: "auto",
-  animate: window && window.navigator.userAgent.indexOf("MSIE") === -1,
-  wrap: false,
-  weekNumbers: false,
+  _disable: [],
+  _enable: [],
   allowInput: false,
-  clickOpens: true,
-  closeOnSelect: true,
-  time_24hr: false,
-  enableTime: false,
-  noCalendar: false,
-  dateFormat: "Y-m-d",
-  ariaDateFormat: "F j, Y",
+  altFormat: "F j, Y",
   altInput: false,
   altInputClass: "form-control input",
-  altFormat: "F j, Y",
-  getWeek,
-  _enable: [],
-  enable: [],
-  _disable: [],
-  disable: [],
-  shorthandCurrentMonth: false,
-  inline: false,
-  static: false,
-  prevArrow:
-    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
-  nextArrow:
-    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
-  enableSeconds: false,
-  hourIncrement: 1,
-  minuteIncrement: 5,
+  animate: window && window.navigator.userAgent.indexOf("MSIE") === -1,
+  ariaDateFormat: "F j, Y",
+  clickOpens: true,
+  closeOnSelect: true,
+  dateFormat: "Y-m-d",
   defaultHour: 12,
   defaultMinute: 0,
   defaultSeconds: 0,
+  disable: [],
   disableMobile: false,
-  locale: "default",
-  plugins: [],
+  enable: [],
+  enableSeconds: false,
+  enableTime: false,
+  getWeek,
+  hourIncrement: 1,
   ignoredFocusElements: [],
-
-  onClose: [],
+  inline: false,
+  locale: "default",
+  minuteIncrement: 5,
+  mode: "single",
+  nextArrow:
+    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+  noCalendar: false,
   onChange: [],
+  onClose: [],
   onDayCreate: [],
+  onDestroy: [],
+  onKeyDown: [],
   onMonthChange: [],
   onOpen: [],
   onParseConfig: [],
   onReady: [],
   onValueUpdate: [],
   onYearChange: [],
-  onKeyDown: [],
-  onDestroy: [],
+  plugins: [],
+  position: "auto",
+  prevArrow:
+    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
+  shorthandCurrentMonth: false,
+  static: false,
+  time_24hr: false,
+  weekNumbers: false,
+  wrap: false,
 };
