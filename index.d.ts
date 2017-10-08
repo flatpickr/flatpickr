86fc071fc897f73023eff285b0f0e1258165e2df
Gregory <gregory.mkv@gmail.com>
2017-10-08 14:27:48 -0400

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ import { FlatpickrFn } from "./src/types/instance";
 export { Instance } from "./src/types/instance";
 export { Options, Plugin } from "./src/types/options";
 
-export default FlatpickrFn;
+declare var flatpickr: FlatpickrFn;
+
+export default flatpickr;
