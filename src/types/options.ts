09839f612e472271492cb9ccc56167f1da97d355
Gregory <gregory.mkv@gmail.com>
2017-10-08 18:32:07 -0400

    Fix config.positionElement option

--- src/types/options.ts
+++ src/types/options.ts
@@ -266,7 +266,7 @@ export interface ParsedOptions {
   parseDate?: Options["parseDate"];
   plugins: Plugin[];
   position: "auto" | "below" | "above";
-  positionElement: HTMLElement;
+  positionElement?: HTMLElement;
   prevArrow: string;
   shorthandCurrentMonth: boolean;
   static: boolean;
@@ -319,7 +319,7 @@ export const defaults: ParsedOptions = {
   onYearChange: [],
   plugins: [],
   position: "auto",
-  positionElement: window.document.body,
+  positionElement: undefined,
   prevArrow:
     "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
   shorthandCurrentMonth: false,
