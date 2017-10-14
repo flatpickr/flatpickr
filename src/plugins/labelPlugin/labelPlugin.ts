ee0cd0bb3ff1c0421b7ce25ac7d9c2d18c2c74f7
goszczynskip <goszczynskip@gmail.com>
2017-10-14 15:12:18 +0200

    fixes labelPlugin on mobile

--- src/plugins/labelPlugin/labelPlugin.ts
+++ src/plugins/labelPlugin/labelPlugin.ts
@@ -5,7 +5,15 @@ function labelPlugin() {
     return {
       onReady() {
         const id = fp.input.id;
-        if (fp.altInput && id) {
+
+        if (!id) {
+          return;
+        }
+
+        if (fp.mobileInput) {
+          fp.input.removeAttribute("id");
+          fp.mobileInput.id = id;
+        } else if (fp.altInput) {
           fp.input.removeAttribute("id");
           fp.altInput.id = id;
         }
