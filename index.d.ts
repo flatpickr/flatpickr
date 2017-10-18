8c55b50373e42e4eeb386112b5e0ec578298e229
Pierre <pierre-H@users.noreply.github.com>
2017-10-18 18:05:58 +0200

    Typing : export all options type (#1076)
    Useful when we need for example to do :
    ```ts
    import { Hook } from 'flatpickr';
    ```

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { FlatpickrFn } from "./src/types/instance";
 export { Instance } from "./src/types/instance";
-export { Options, Plugin } from "./src/types/options";
+export * from "./src/types/options";
 
 declare var flatpickr: FlatpickrFn;
 
