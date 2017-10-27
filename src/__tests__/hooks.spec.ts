25e361311fbb0e2690942fc77c3511dba7234f74
Gregory <gregory.mkv@gmail.com>
2017-10-27 13:56:15 -0400

    Add unit test for onOpen

---
+++ src/__tests__/hooks.spec.ts
@@ -0,0 +1,27 @@
+import flatpickr from "index";
+import { Options } from "types/options";
+import { Instance } from "types/instance";
+
+flatpickr.defaultConfig.animate = false;
+
+jest.useFakeTimers();
+
+const createInstance = (config?: Options): Instance => {
+  return flatpickr(
+    document.createElement("input"),
+    config as Options
+  ) as Instance;
+};
+
+describe("Events + Hooks", () => {
+  it("should fire onOpen only once", () => {
+    let timesFired = 0;
+
+    const fp = createInstance({
+      onOpen: () => timesFired++,
+    });
+
+    fp.open();
+    expect(timesFired).toEqual(1);
+  });
+});
