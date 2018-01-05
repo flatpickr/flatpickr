d6b0dfff5190eced253265732e9be198293f4729
Gregory <gregory.mkv@gmail.com>
2018-01-04 23:46:01 -0500

    Update getWeek() implementation | resolves #1167

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -143,12 +143,24 @@ export const monthToStr = (
 ) => locale.months[shorthand ? "shorthand" : "longhand"][monthNumber];
 
 export const getWeek = (givenDate: Date) => {
-  const onejan = new Date(givenDate.getFullYear(), 0, 1);
-  return Math.ceil(
-    ((givenDate.getTime() - onejan.getTime()) / 86400000 +
-      onejan.getDay() +
-      1) /
-      7
+  const date = new Date(givenDate.getTime());
+  date.setHours(0, 0, 0, 0);
+
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        (week1.getDay() + 6) % 7) /
+        7
+    )
   );
 };
 
