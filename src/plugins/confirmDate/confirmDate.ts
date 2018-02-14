e0258fe24b25eab04f4ec098f3883d2a111627b4
Gregory <gregory.mkv@gmail.com>
2018-02-13 23:51:13 -0500

    Cleanup confirmDatePlugin resolves #1259

--- src/plugins/confirmDate/confirmDate.ts
+++ src/plugins/confirmDate/confirmDate.ts
@@ -21,12 +21,8 @@ function confirmDatePlugin(pluginConfig: Config): Plugin {
   let confirmContainer: HTMLDivElement;
 
   return function(fp: Instance) {
-    if (fp.calendarContainer === undefined) {
-      // disable on mobile
-      return {};
-    }
-
-    const hooks = {
+    if (fp.config.noCalendar || fp.isMobile) return {};
+    return {
       onKeyDown(_: Date[], __: string, ___: Instance, e: KeyboardEvent) {
         if (fp.config.enableTime && e.key === "Tab" && e.target === fp.amPM) {
           e.preventDefault();
@@ -52,7 +48,7 @@ function confirmDatePlugin(pluginConfig: Config): Plugin {
       },
       ...(!config.showAlways
         ? {
-            onChange: function(_: Date, dateStr: string) {
+            onChange: function(_: Date[], dateStr: string) {
               const showCondition =
                 fp.config.enableTime || fp.config.mode === "multiple";
               if (dateStr && !fp.config.inline && showCondition)
@@ -62,8 +58,6 @@ function confirmDatePlugin(pluginConfig: Config): Plugin {
           }
         : {}),
     };
-
-    return hooks;
   };
 }
 
