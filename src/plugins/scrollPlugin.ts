34a0b3a893f5e3f2e3b187cc268e0dc013fde8b9
Gregory <gregory.mkv@gmail.com>
2018-03-23 15:57:07 -0400

    Add scrollPlugin | resolves #1291

---
+++ src/plugins/scrollPlugin.ts
@@ -0,0 +1,58 @@
+import { Plugin } from "../types/options";
+import { Instance } from "../types/instance";
+function delta(e: WheelEvent) {
+  return Math.max(-1, Math.min(1, e.wheelDelta || -e.deltaY));
+}
+
+const scroll = e => {
+  e.preventDefault();
+  const ev = new CustomEvent("increment", {
+    bubbles: true,
+  });
+  (<any>ev).delta = delta(e);
+  (<HTMLInputElement>e.target).dispatchEvent(ev);
+};
+
+function scrollMonth(fp: Instance) {
+  return (e: WheelEvent) => {
+    e.preventDefault();
+    const mDelta = delta(e);
+    fp.changeMonth(mDelta);
+  };
+}
+
+function scrollPlugin(): Plugin {
+  return function(fp) {
+    const monthScroller = scrollMonth(fp);
+    return {
+      onReady() {
+        if (fp.timeContainer) {
+          fp.timeContainer.addEventListener("wheel", scroll);
+        }
+
+        fp.yearElements.forEach(yearElem =>
+          yearElem.addEventListener("wheel", scroll)
+        );
+
+        fp.monthElements.forEach(monthElem =>
+          monthElem.addEventListener("wheel", monthScroller)
+        );
+      },
+      onDestroy() {
+        if (fp.timeContainer) {
+          fp.timeContainer.removeEventListener("wheel", scroll);
+        }
+
+        fp.yearElements.forEach(yearElem =>
+          yearElem.removeEventListener("wheel", scroll)
+        );
+
+        fp.monthElements.forEach(monthElem =>
+          monthElem.removeEventListener("wheel", monthScroller)
+        );
+      },
+    };
+  };
+}
+
+export default scrollPlugin;
