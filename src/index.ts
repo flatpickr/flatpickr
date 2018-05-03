db29971a2bf1f5bf1d4522ce32ed15a96d538093
Gregory <gregory.mkv@gmail.com>
2018-05-03 12:39:28 -0400

    setCalendarWidth() - simplify

--- src/index.ts
+++ src/index.ts
@@ -128,23 +128,24 @@ function FlatpickrInstance(
   }
 
   function setCalendarWidth() {
-    if (self.config.weekNumbers === false && self.config.showMonths === 1)
-      return;
-    else if (self.daysContainer !== undefined) {
+    const config = self.config;
+
+    if (config.weekNumbers === false && config.showMonths === 1) return;
+    else if (config.noCalendar !== true) {
       window.requestAnimationFrame(function() {
         self.calendarContainer.style.visibility = "hidden";
         self.calendarContainer.style.display = "block";
         if (self.daysContainer !== undefined) {
-          const daysWidth =
-            (self.days.offsetWidth + 1) * self.config.showMonths;
+          const daysWidth = (self.days.offsetWidth + 1) * config.showMonths;
 
           self.daysContainer.style.width = daysWidth + "px";
-          self.calendarContainer.style.width = daysWidth + "px";
 
-          if (self.weekWrapper !== undefined) {
-            self.calendarContainer.style.width =
-              daysWidth + self.weekWrapper.offsetWidth + "px";
-          }
+          self.calendarContainer.style.width =
+            daysWidth +
+            (self.weekWrapper !== undefined
+              ? self.weekWrapper.offsetWidth
+              : 0) +
+            "px";
 
           self.calendarContainer.style.removeProperty("visibility");
           self.calendarContainer.style.removeProperty("display");
@@ -322,7 +323,12 @@ function FlatpickrInstance(
       return element.forEach(el => bind(el, event, handler, options));
 
     element.addEventListener(event, handler as EventListener, options);
-    self._handlers.push({ element: element as Element, event, handler });
+    self._handlers.push({
+      element: element as Element,
+      event,
+      handler,
+      options,
+    });
   }
 
   /**
@@ -1197,7 +1203,11 @@ function FlatpickrInstance(
 
     for (let i = self._handlers.length; i--; ) {
       const h = self._handlers[i];
-      h.element.removeEventListener(h.event, h.handler as EventListener);
+      h.element.removeEventListener(
+        h.event,
+        h.handler as EventListener,
+        h.options
+      );
     }
 
     self._handlers = [];
