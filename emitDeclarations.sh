8169683c2996ce67ecc9c29030e7c7f10a744d70
Maxim Matyunin <krabovm@gmail.com>
2018-09-06 10:42:54 +0400

    fix: modify for ubuntu

--- emitDeclarations.sh
+++ emitDeclarations.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 ./node_modules/typescript/bin/tsc -p tsconfig.declarations.json
 os=`uname`
-if [[ "$os" == "Darwin" ]]; then
-    cp -R types dist
-else
-    cp -RT types dist
-fi
+cp -RT types dist
 cp src/typings.d.ts dist/typings.d.ts
 rm -rf types
 
