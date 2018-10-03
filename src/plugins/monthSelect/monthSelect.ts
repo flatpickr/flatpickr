824b44a694ae8c3c86f3fa7d5a092c4f3e1c6cf0
Andrea Minato <aminato@tempestive.com>
2018-10-03 11:54:08 +0200

    fix monthSelection working only first time

--- src/plugins/monthSelect/monthSelect.ts
+++ src/plugins/monthSelect/monthSelect.ts
@@ -8,15 +8,14 @@ export interface ExtraFields {
 
 function monthSelectPlugin(): Plugin<ExtraFields> {
   return function(fp) {
-    var days: NodeListOf<DayElement>;
-
     function onDayHover(event: MouseEvent) {
       if (
         !event.target ||
         !(event.target as Element).classList.contains("flatpickr-day")
       )
         return;
-
+      var days: NodeListOf<DayElement>;
+      days = fp.days.childNodes as NodeListOf<DayElement>;
       const dayIndex = Array.prototype.indexOf.call(days, event.target);
       fp.monthStartDay = new Date(
         days[dayIndex].dateObj.getFullYear(),
@@ -50,40 +49,60 @@ function monthSelectPlugin(): Plugin<ExtraFields> {
     }
 
     function highlightMonth() {
-      for (var i = days.length; i--; ) {
-        var date = days[i].dateObj.getTime();
-        if (date >= fp.monthStartDay && date <= fp.monthEndDay)
-          days[i].classList.add("month", "selected");
-        if (date != fp.monthEndDay) days[i].classList.remove("endRange");
-        else days[i].classList.add("endRange");
-        if (date != fp.monthStartDay) days[i].classList.remove("startRange");
-        else days[i].classList.add("startRange");
+      var selDate: Date | undefined = fp.latestSelectedDateObj;
+      if (
+        selDate !== undefined &&
+        selDate.getMonth() === fp.currentMonth &&
+        selDate.getFullYear() === fp.currentYear &&
+        selDate.getMonth() === new Date(fp.monthStartDay).getMonth() &&
+        selDate.getMonth() === new Date(fp.monthEndDay).getMonth()
+      ) {
+        var days: NodeListOf<DayElement>;
+        days = fp.days.childNodes as NodeListOf<DayElement>;
+        for (var i = days.length; i--; ) {
+          var date = days[i].dateObj.getTime();
+          if (date >= fp.monthStartDay && date <= fp.monthEndDay)
+            days[i].classList.add("month", "selected");
+          if (date != fp.monthEndDay) days[i].classList.remove("endRange");
+          else days[i].classList.add("endRange");
+          if (date != fp.monthStartDay) days[i].classList.remove("startRange");
+          else days[i].classList.add("startRange");
+        }
       }
     }
 
     function clearHover() {
+      var days: NodeListOf<DayElement>;
+      days = fp.days.childNodes as NodeListOf<DayElement>;
       for (var i = days.length; i--; ) {
         days[i].classList.remove("inRange");
       }
     }
 
+    function onReady() {
+      if (fp.daysContainer) {
+        fp.daysContainer.addEventListener("mouseover", onDayHover);
+      }
+    }
+
+    function onDestroy() {
+      if (fp.daysContainer) {
+        fp.daysContainer.removeEventListener("mouseover", onDayHover);
+      }
+    }
+
     return {
+      onValueUpdate: highlightMonth,
       onChange: highlightMonth,
       onMonthChange: highlightMonth,
+      onYearChange: highlightMonth,
       onClose: clearHover,
       onParseConfig: function onParseConfig() {
         fp.config.mode = "single";
         fp.config.enableTime = false;
       },
-      onReady: [
-        function() {
-          days = fp.days.childNodes as NodeListOf<DayElement>;
-        },
-        function() {
-          return fp.days.addEventListener("mouseover", onDayHover);
-        },
-        highlightMonth,
-      ],
+      onReady: [onReady, highlightMonth],
+      onDestroy: onDestroy,
     };
   };
 }
