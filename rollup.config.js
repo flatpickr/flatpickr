b6eeada263b81363eff24405d12f09f91a7b6890
Greg <gregory.mkv@gmail.com>
2019-02-16 14:09:25 -0500

    Upgrade & cleanup build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,11 @@
-// rollup.config.js
-import typescript from 'rollup-plugin-typescript';
-import babel from 'rollup-plugin-babel';
-import serve from 'rollup-plugin-serve';
-import livereload from "rollup-plugin-livereload"
+const tsc = require("typescript")
+const rollupConfig = require("fs").readFileSync("./config/rollup.ts", "utf8")
 
-const pkg = require("./package.json");
-import path from "path";
-
-export default {
-  input: './src/index.ts',
-  output: {
-    file: 'dist/flatpickr.js',
-    name: "flatpickr",
-    format: 'umd',
-    exports: "default",
-    banner: `/* flatpickr v${pkg.version}, @license MIT */`
-  },
-
-  plugins: [
-    typescript({
-      tsconfig: path.resolve("src/tsconfig.json"),
-      typescript: require('typescript')
-    }),
-    babel({
-      runtimeHelpers: true
-    }),
-    ...process.env.ROLLUP_WATCH ? [serve({
-      open: true,
-      contentBase: '',
-      host: '0.0.0.0',
-      port: 8000,
-    }),
-    livereload()] : []
-  ],
-  watch: {
-      chokidar: false
-    }
+const options = {
+  "compilerOptions": {
+    "target": "es5",
+    "module": "commonjs"
+  }
 }
+
+export default eval(tsc.transpileModule(rollupConfig, options).outputText)
