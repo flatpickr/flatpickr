3e3c2cfccf8ef5726ad6ae965e63362c906dc64d
Soundar <sdjthedeveloper@gmail.com>
2019-04-01 18:24:57 +0530

    Modified destroy method for bug #1641  There is no need to empty input element value on destroying flatpickr instance

--- src/index.ts
+++ src/index.ts
@@ -1279,7 +1279,6 @@ function FlatpickrInstance(
       self.input.type = (self.input as any)._type;
       self.input.classList.remove("flatpickr-input");
       self.input.removeAttribute("readonly");
-      self.input.value = "";
     }
 
     ([
