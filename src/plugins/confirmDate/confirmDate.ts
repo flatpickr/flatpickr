50ec292ea34ac94ac1de16dc16eb9dcea38580ee
Viorel Sfetea <viorel.sfetea@gmail.com>
2019-04-04 17:06:59 +0200

    Removed left over debug code

--- src/plugins/confirmDate/confirmDate.ts
+++ src/plugins/confirmDate/confirmDate.ts
@@ -52,10 +52,6 @@ function confirmDatePlugin(pluginConfig: Config): Plugin {
       ...(!config.showAlways
         ? {
             onChange: function(_: Date[], dateStr: string) {
-              fp.config.plugins.forEach(test => {
-                console.log("confirmDate.ts:53", test);
-              });
-
               const showCondition =
                 fp.config.enableTime || fp.config.mode === "multiple";
 
