397056bd483c9455a440731a1f058e21e58b452c
vindeo47 <55259875+vindeo47@users.noreply.github.com>
2019-09-12 22:27:29 +0000

    added greek translation

---
+++ src/l10n/el.ts
@@ -0,0 +1,67 @@
+import { Locale } from "../types/locale";
+
+export const greek: Locale = {
+  weekdays: {
+    shorthand: ["Κυρ","Δευ","Τρι","Τετ","Πεμ","Παρ","Σαβ"],
+    longhand: [
+      "Κυριακή",
+      "Δευτέρα",
+      "Τρίτη",
+      "Τετάρτη",
+      "Πέμπτη",
+      "Παρασκευή",
+      "Σάββατο",
+    ],
+  },
+  months: {
+    shorthand: [
+      "Ιαν",
+      "Φεβ",
+      "Μαρ",
+      "Απρ",
+      "Μαϊ",
+      "Ιουν",
+      "Ιουλ",
+      "Αυγ",
+      "Σεπ",
+      "Οκτ",
+      "Νοε",
+      "Δεκ",
+    ],
+      
+    longhand: [
+      "Ιανουάριος",
+      "Φεβρουάριος",
+      "Μάρτιος",
+      "Απρίλιος",
+      "Μάιος",
+      "Ιούνιος",
+      "Ιούλιος",
+      "Αύγουστος",
+      "Σεπτέμβριος",
+      "Οκτώβριος",
+      "Νοέμβριος",
+      "Δεκέμβριος",
+    ],
+      
+  },
+      
+  daysInMonth: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+  firstDayOfWeek: 0,
+  ordinal: (nth: number) => {
+        return "η";
+  },
+  rangeSeparator: " ως ",
+  weekAbbreviation: "Εβ",
+  scrollTitle: "Σύρατε για αύξηση",
+  toggleTitle: "Κλικ για εναλλαγή",
+  amPM: ["πμ", "μμ"],
+  yearAriaLabel: "Έτος",
+  hourAriaLabel: "Ώρα",
+  minuteAriaLabel: "Λεπτό",
+  time_24hr: true,
+  firstDayOfWeek: 1, // Monday
+};
+
+
+export default greek;
