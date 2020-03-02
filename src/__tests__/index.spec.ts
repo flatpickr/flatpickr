7dff8c60efe754729ac3ccaae5aca73df29d9e4f
Faragó Márton <marton@megagiga.hu>
2020-03-02 16:15:43 +0100

    Fix mobile datepickers when used with a keyboard (#2120)
    * fix formatter for years shorter than 4 digits
    
    * do not use user provided formatter when on mobile
    
    on mobile, the user provided formatter must not overload the format needed to populate date/time/datetime-local inputs.

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -350,19 +350,30 @@ describe("flatpickr", () => {
   describe("date formatting", () => {
     describe("default formatter", () => {
       const DEFAULT_FORMAT_1 = "d.m.y H:i:S",
-        DEFAULT_FORMAT_2 = "D j F, 'y";
+        DEFAULT_FORMAT_2 = "D j F, 'y",
+        DEFAULT_FORMAT_3 = 'Y-m-d';
 
-      it(`should format the date with the pattern "${DEFAULT_FORMAT_1}"`, () => {
-        const RESULT = "20.10.16 09:19:59";
-        createInstance({
-          dateFormat: DEFAULT_FORMAT_1,
+        it(`should format the date with the pattern "${DEFAULT_FORMAT_1}"`, () => {
+          const RESULT = "20.10.16 09:19:59";
+          createInstance({
+            dateFormat: DEFAULT_FORMAT_1,
+          });
+  
+          fp.setDate("20.10.16 09:19:59");
+          expect(fp.input.value).toEqual(RESULT);
+          fp.setDate("2015.11.21 19:29:49");
+          expect(fp.input.value).not.toEqual(RESULT);
         });
 
-        fp.setDate("20.10.16 09:19:59");
-        expect(fp.input.value).toEqual(RESULT);
-        fp.setDate("2015.11.21 19:29:49");
-        expect(fp.input.value).not.toEqual(RESULT);
-      });
+        it("should format dates for year 0001", () => {
+          const RESULT = "0001-07-15";
+          createInstance({
+            dateFormat: DEFAULT_FORMAT_3,
+          });
+  
+          fp.setDate("0001-07-15");
+          expect(fp.input.value).toEqual(RESULT);
+        });
 
       it(`should format the date with the pattern "${DEFAULT_FORMAT_2}"`, () => {
         const RESULT = "Thu 20 October, '16";
