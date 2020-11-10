cd1d18125b206de60d39d78f94cbf25eabde119b
NELSON COSTA <nelson.nit@gmail.com>
2020-11-10 10:07:28 -0500

    Create pt_BR.ts

---
+++ src/l10n/pt_BR.ts
@@ -0,0 +1,63 @@
+/* Portuguese_BR locals for flatpickr */
+import { CustomLocale } from "../types/locale";
+import { FlatpickrFn } from "../types/instance";
+
+const fp =
+  typeof window !== "undefined" && window.flatpickr !== undefined
+    ? window.flatpickr
+    : ({
+        l10ns: {},
+      } as FlatpickrFn);
+
+export const Portuguese_BR: CustomLocale = {
+  weekdays: {
+    shorthand: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    longhand: [
+      "Domingo",
+      "Segunda-feira",
+      "Terça-feira",
+      "Quarta-feira",
+      "Quinta-feira",
+      "Sexta-feira",
+      "Sábado",
+    ],
+  },
+
+  months: {
+    shorthand: [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
+    ],
+    longhand: [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ],
+  },
+
+  rangeSeparator: " até ",
+  time_24hr: true,
+};
+
+fp.l10ns.pt = Portuguese_BR;
+
+export default fp.l10ns;
