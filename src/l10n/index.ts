1850305cdf51e49ae3b483c23635835d6e78c8b3
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2022-03-13 17:44:22 +0900

    Run Prettier check in the CI to reject unformatted code

--- src/l10n/index.ts
+++ src/l10n/index.ts
@@ -42,7 +42,7 @@ import { Mongolian as mn } from "./mn";
 import { Malaysian as ms } from "./ms";
 import { Burmese as my } from "./my";
 import { Dutch as nl } from "./nl";
-import { NorwegianNynorsk as nn } from './nn';
+import { NorwegianNynorsk as nn } from "./nn";
 import { Norwegian as no } from "./no";
 import { Punjabi as pa } from "./pa";
 import { Polish as pl } from "./pl";
