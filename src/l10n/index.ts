609911f70c83e593a01600e277f1d481065c0f6c
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2022-03-30 17:47:08 +0300

    chore: Renamed old gr references

--- src/l10n/index.ts
+++ src/l10n/index.ts
@@ -21,7 +21,7 @@ import { Persian as fa } from "./fa";
 import { Finnish as fi } from "./fi";
 import { Faroese as fo } from "./fo";
 import { French as fr } from "./fr";
-import { Greek as gr } from "./gr";
+import { Greek as el } from "./el";
 import { Hebrew as he } from "./he";
 import { Hindi as hi } from "./hi";
 import { Croatian as hr } from "./hr";
@@ -88,7 +88,7 @@ const l10n: Record<key, CustomLocale> = {
   fi,
   fo,
   fr,
-  gr,
+  el,
   he,
   hi,
   hr,
